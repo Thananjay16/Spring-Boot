e1711b6f6c7e3c97f3bc55803f7aabef4f27fd34
Thananjay16 <153717341+Thananjay16@users.noreply.github.com>
2024-04-27 14:22:12 +0530

    Add files via upload

---
+++ Project/project/src/main/java/com/example/project/model/Homeappliance.java
@@ -0,0 +1,41 @@
+package com.example.project.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Homeappliance{
+    @Id
+    int CustomerId;
+    String CustomerName;
+    String ServiceCenter;
+    String Contact;
+
+    public int getCustomerId() {
+        return CustomerId;
+    }
+    public void setCustomerId(int customerId) {
+        CustomerId = customerId;
+    }
+    public String getCustomerName() {
+        return CustomerName;
+    }
+    public void setCustomerName(String customerName) {
+        CustomerName = customerName;
+    }
+    public String getServiceCenter() {
+        return ServiceCenter;
+    }
+    public void setServiceCenter(String serviceCenter) {
+        ServiceCenter = serviceCenter;
+    }
+    public String getContact() {
+        return Contact;
+    }
+    public void setContact(String contact) {
+        Contact = contact;
+    }
+
+
+}
